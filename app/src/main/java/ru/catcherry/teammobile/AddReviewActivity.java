e560f813e10cdb080b3b78fc69c28d91328451bd
Darya Belyaeva <d.belyaeva1@gmail.com>
2020-12-08 16:10:48 +0400

    Создание активити AddReviewActivity, привязка фаба к нему

---
+++ app/src/main/java/ru/catcherry/teammobile/AddReviewActivity.java
@@ -0,0 +1,14 @@
+package ru.catcherry.teammobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_review);
+    }
+}
